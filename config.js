845c256972b8e34d7e1f416ae07bb0dd654b91b6
vincenzo-afk <itsmebk2007@gmail.com>
2026-05-12 18:14:17 +0530

    Add real API keys to private repository

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-const CONFIG = {
-  groqKey: "",
-  shodanKey: "",
-  vtKey: "",
-  abuseKey: "",
-  urlscanKey: ""
+const SECRETS = {
+  groqKey: "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",
+  shodanKey: "Mln3XbB6a2XQwadyS5JVhDb0GbeCfYBR",
+  vtKey: "bfc5de2008d6d138eea01e53df8f8720a5df83ad8f0ff1d23a5835b287e5433b",
+  abuseKey: "b56c034d69f95963ecd44ed774afc0289a3afa653a9284f953604f96227786031d4528bfbcbc8adc",
+  urlscanKey: "019e1c1b-ac0c-725c-baec-17b9754c24eb"
 };
