37ac591f45d69ccbbdaa86d17fbc33239c67a71f
vincenzo-afk <itsmebk2007@gmail.com>
2026-05-12 19:06:46 +0530

    Cleanup: Remove hibpKey and duplicate abuseipdbKey from config.js

--- config.js
+++ config.js
@@ -4,7 +4,5 @@ const SECRETS = {
   vtKey: "bfc5de2008d6d138eea01e53df8f8720a5df83ad8f0ff1d23a5835b287e5433b",
   abuseKey: "b56c034d69f95963ecd44ed774afc0289a3afa653a9284f953604f96227786031d4528bfbcbc8adc",
   urlscanKey: "019e1c1b-ac0c-725c-baec-17b9754c24eb",
-  ipinfoToken: "YOUR_IPINFO_TOKEN",
-  hibpKey: "YOUR_HIBP_API_KEY",
-  abuseipdbKey: "YOUR_ABUSEIPDB_KEY",
+  ipinfoToken: "78a9d1c2de2928",
 };
