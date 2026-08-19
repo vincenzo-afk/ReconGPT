ddda36c40e3d627f2a89516aa63cb31b7d90acfb
vincenzo-afk <itsmebk2007@gmail.com>
2026-08-19 17:49:07 +0000

    fix: run Vercel API bundle as ESM

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
-import bundledApi from "./app.cjs";
+import { createApiApp } from "./app.mjs";
 
 /**
  * Vercel recognizes this JavaScript module as the function entrypoint. The
  * generated CommonJS bundle contains the complete local API import graph.
  */
-export default bundledApi.createApiApp();
+export default createApiApp();
